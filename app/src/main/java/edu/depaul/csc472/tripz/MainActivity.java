6a85c7d6afc54c7d1a64df179ba30b012311009f
matheuscz <mahecaza@hotmail.com>
2015-11-18 18:29:24 -0600

    Vamos ver se da certo agora.

--- app/src/main/java/edu/depaul/csc472/tripz/MainActivity.java
+++ app/src/main/java/edu/depaul/csc472/tripz/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    int i = 0;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
